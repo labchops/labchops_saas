8b162df4bbb7f1102889eb046cab971ab79a781a
Aaron Roberts <aaron.roberts47@gmail.com>
2017-05-24 20:54:39 +0000

    Created form post message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,17 @@ class ContactsController < ApplicationController
     end
 
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            redirect_to new_contact_path, notice: "Successful :)"
+        else
+            redirect_to new_contact_path, notice: "What you trying to do bruh???"
+        end   
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
