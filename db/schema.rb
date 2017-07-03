65a83048a5e830d1a4d11fafd9e3ab2bf75243bc
Aaron Roberts <aaron.roberts47@gmail.com>
2017-07-03 18:48:32 +0000

    Installed devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608180952) do
+ActiveRecord::Schema.define(version: 20170703165021) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20170608180952) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
