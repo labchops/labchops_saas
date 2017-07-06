8d3f92192e44b1a15dcfefefcdf1af6e878fcf81
Aaron Roberts <aaron.roberts47@gmail.com>
2017-07-06 16:53:56 +0000

    Added plan model and db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-#ActiveRecord::Schema.define(version: 20170524192627) do
-ActiveRecord::Schema.define(version: 20170703165021) do
+ActiveRecord::Schema.define(version: 20170706164054) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -22,27 +21,11 @@ ActiveRecord::Schema.define(version: 20170703165021) do
     t.datetime "updated_at"
   end
 
-
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-
-
 
 end
