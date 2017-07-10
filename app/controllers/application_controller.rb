e37e3293fca44525ced397a65dcd930eb61e7ff2
Aaron Roberts <aaron.roberts47@gmail.com>
2017-07-10 20:16:54 +0000

    Updated application.html and application_controller to fixed the user.js errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,6 @@ class ApplicationController < ActionController::Base
   
   protected
     def configure_permitted_parameters
-      # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) } #Deprecated in Devise 4.1
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
-      # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :email, bank_attributes: [:bank_name, :bank_account]])
-
+      devise_parameter_sanitizer.for(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
     end
 end
