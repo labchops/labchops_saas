adace34d4c2cb52dae28ce437508e901c742bbd2
Aaron Roberts <aaron.roberts47@gmail.com>
2017-07-11 20:19:42 +0000

    Cleaned up the pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :contacts
   get '/about' => 'pages#about'
-  get '/mychops' => 'pages#mychops'
+  #get '/mychops' => 'pages#mychops'
 
   root'pages#home'
   
